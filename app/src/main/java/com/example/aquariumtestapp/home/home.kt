592b9f839fc0fc8fca104c1de19fce8d04999d18
alexandrebnlt <103410791+alexandrebnlt@users.noreply.github.com>
2024-10-04 09:09:21 +0200

    start the page home with the circle img

--- app/src/main/java/com/example/aquariumtestapp/home/home.kt
+++ app/src/main/java/com/example/aquariumtestapp/home/home.kt
@@ -1,31 +1,57 @@
 package com.example.aquariumtestapp.home
 
-import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import com.example.aquariumtestapp.camera.CameraActivity
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.aquariumtestapp.R
+
 
 @Composable
-fun home () {
+fun Home () {
     val context = LocalContext.current
+    BlocHautStart()
+}
 
-
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
+@Composable
+fun BlocHautStart() {
+    Row(
+        modifier = Modifier.padding(16.dp)
     ) {
-        /*Button(onClick = {
-            // Lancer CameraActivity
-            context.startActivity(Intent(context, CameraActivity::class.java))
-        }) {
-            Text("Open Camera")
-        }*/
-        Text(text = "home")
+        CircularImage()
+        Column {
+            Text(text = "Bonjour Jamie", modifier = Modifier.padding(8.dp))
+            Text(text = "Explore Task", modifier = Modifier.padding(8.dp))
+        }
+
+        CircularImage()
     }
+
+}
+
+
+@Composable
+fun CircularImage() {
+
+    Image(
+        painter = rememberAsyncImagePainter(R.drawable.test),
+        contentDescription = null,
+        modifier = Modifier
+            .size(80.dp) // Dimension de l'image
+            .clip(CircleShape) // Appliquer une forme circulaire
+            .border(1.dp, androidx.compose.ui.graphics.Color.Gray, CircleShape) // Ajouter une bordure
+    )
 }
