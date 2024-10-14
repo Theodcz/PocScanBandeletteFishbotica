8facda4fcf765c7204f017fcabf377465e0a2d50
alexandrebnlt <103410791+alexandrebnlt@users.noreply.github.com>
2024-10-14 10:49:14 +0200

    FIX : check is user login at the first page

--- app/src/main/java/com/example/aquariumtestapp/PageBienvenue.kt
+++ app/src/main/java/com/example/aquariumtestapp/PageBienvenue.kt
@@ -16,84 +16,135 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.aquariumtestapp.data.model.UserState
 
 @Composable 
-fun PageBienvenue(navController: NavHostController) {
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 160.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(painter = painterResource(id = R.drawable.aquarium),
+fun PageBienvenue(viewModel: SupabaseAuthViewModel = viewModel(), navController: NavHostController) {
+    val context = LocalContext.current
+    val userState by viewModel.userState
+    var isLoading by remember { mutableStateOf(true) }
+    var hasNavigated by remember { mutableStateOf(false) } // Variable pour suivre la navigation
+
+    LaunchedEffect(Unit) {
+        viewModel.isUserLoggedIn(context)
+    }
+
+    when (userState) {
+        is UserState.Loading -> {
+            LoadingComponent()
+        }
+
+        is UserState.Success -> {
+            val message = (userState as UserState.Success).message
+            if (message == "User already logged in!" && !hasNavigated) {
+                println("Navigating to bottomAppBar")
+                hasNavigated = true // Met à jour la variable pour éviter des navigations répétées
+                navController.navigate("bottomAppBar")
+            } else {
+                isLoading = false // L'utilisateur n'est pas connecté, on arrête le chargement
+            }
+        }
+
+        is UserState.Error -> {
+            Text("Erreur : ${(userState as UserState.Error).message}")
+        }
+    }
+
+    if (!isLoading && !hasNavigated) {
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 160.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.aquarium),
                 contentDescription = "Aquarium",
                 modifier = Modifier
                     .size(272.dp, 260.dp)
-        )
-        Row (
-            modifier = Modifier.padding(top = 10.dp)
-        ){
-            Box(
-                modifier = Modifier
-                    .size(13.dp) // Taille du cercle (diamètre)
-                    .background(color = Color(0xFF0B7FB5), shape = CircleShape) // Couleur et forme
+            )
+            Row(
+                modifier = Modifier.padding(top = 10.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(13.dp) // Taille du cercle (diamètre)
+                        .background(
+                            color = Color(0xFF0B7FB5),
+                            shape = CircleShape
+                        ) // Couleur et forme
+                )
+
+                Spacer(modifier = Modifier.padding(5.dp))
+
+                Box(
+                    modifier = Modifier
+                        .size(13.dp) // Taille du cercle (diamètre)
+                        .background(
+                            color = Color(0xFFE7F3F8),
+                            shape = CircleShape
+                        ) // Couleur et forme
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            Text(
+                text = "Bienvenue chez FISHBOTICA",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold
             )
 
-            Spacer(modifier = Modifier.padding(5.dp))
+            Spacer(modifier = Modifier.padding(1.dp))
 
-            Box(
+            Text(
+                text = "Explorez une expérience unique de gestion, " +
+                        "conseils et automatisation " +
+                        "pour simplifier votre aventure d'aquariophile.",
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .size(13.dp) // Taille du cercle (diamètre)
-                    .background(color = Color(0xFFE7F3F8), shape = CircleShape) // Couleur et forme
-            )
-        }
-
-        Spacer(modifier = Modifier.padding(20.dp))
-
-        Text(
-            text = "Bienvenue chez FISHBOTICA",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.SemiBold
-        )
-
-        Spacer(modifier = Modifier.padding(1.dp))
-
-        Text(text = "Explorez une expérience unique de gestion, " +
-                "conseils et automatisation " +
-                "pour simplifier votre aventure d'aquariophile.",
-            fontSize = 13.sp,
-            modifier = Modifier
-                .padding(top = 15.dp, start = 90.dp, end = 90.dp)
-                .fillMaxWidth(), // Prend toute la largeur de la page
-            textAlign = TextAlign.Center,
-            color = Color.Black.copy(alpha = 0.74f)
-        )
-
-        Spacer(modifier = Modifier.padding(20.dp))
-
-        Button(colors = ButtonDefaults.buttonColors(
-            containerColor = Color(0xFF0B7FB5), // Couleur de fond personnalisée
-            contentColor = Color.White // Couleur du texte dans le bouton
-        ),
-            modifier = Modifier.size(303.dp, 48.dp),
-            shape = RoundedCornerShape(10.dp),
-            onClick = {
-                navController.navigate("login&register")
-            }) {
-            Text(text = "Découvrir",
-                fontSize = 11.sp,
+                    .padding(top = 15.dp, start = 90.dp, end = 90.dp)
+                    .fillMaxWidth(), // Prend toute la largeur de la page
                 textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
+                color = Color.Black.copy(alpha = 0.74f)
             )
+
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            Button(colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF0B7FB5), // Couleur de fond personnalisée
+                contentColor = Color.White // Couleur du texte dans le bouton
+            ),
+                modifier = Modifier.size(303.dp, 48.dp),
+                shape = RoundedCornerShape(10.dp),
+                onClick = {
+                    navController.navigate("login&register")
+                }) {
+                Text(
+                    text = "Découvrir",
+                    fontSize = 11.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
 }
